677024177b8c2a00efc855b3c5981103dd4c9b18
Raffael <132432134+Raffael47@users.noreply.github.com>
2025-07-27 05:54:58 +0700

    feat: update about us align on mobile, remove header subtitle

--- src/features/components/aboutUs/index.tsx
+++ src/features/components/aboutUs/index.tsx
@@ -3,12 +3,14 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Stack, Text } from '@mantine/core';
 import { useTranslations } from 'next-intl';
+import { useMediaQuery } from '@mantine/hooks';
 
 const AboutUsSection: React.FC = () => {
   const t = useTranslations('home.about_us');
 
   const ref = useRef(null);
   const [visible, setVisible] = useState(false);
+  const tablet = useMediaQuery(`(max-width: 991px)`);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -36,7 +38,7 @@ const AboutUsSection: React.FC = () => {
       ref={ref}
       h={{ base: '843.2px', md: '900px', xl: '1281px' }}
       pt={{ base: '88px', md: '115px' }}
-      px="9px"
+      px={{ base: '22px', md: '82px' }}
       style={{
         backgroundImage: 'url(/assets/home/about-us/about-us-background.webp)',
         backgroundRepeat: 'no-repeat',
@@ -44,13 +46,13 @@ const AboutUsSection: React.FC = () => {
         backgroundPosition: 'center',
       }}
       pos="relative">
-      <Stack justify="center" align="center">
+      <Stack justify="center" align={tablet ? 'left' : 'center'}>
         <Text
           fw={400}
           fz={{ base: '32px', md: '64px' }}
           lh="100%"
           c="black"
-          ta="center"
+          ta={{ base: 'left', md: 'center' }}
           style={{
             opacity: visible ? 1 : 0,
             transform: visible ? 'translateY(0px)' : 'translateY(40px)',
@@ -61,7 +63,7 @@ const AboutUsSection: React.FC = () => {
         <Text
           fw={400}
           fz={{ base: '16px', md: '32px' }}
-          ta="center"
+          ta={{ base: 'left', md: 'center' }}
           c="black"
           mt={{ base: '24px', md: '15px' }}
           style={{
@@ -69,8 +71,21 @@ const AboutUsSection: React.FC = () => {
             transform: visible ? 'translateY(0px)' : 'translateY(40px)',
             transition: 'opacity 0.8s ease-out, transform 0.8s ease-out',
           }}
-          maw={{ md: '687px' }}>
-          {t('description_one')} <br />
+          maw={{ sm: '687px' }}>
+          {t('description_one')}
+        </Text>
+        <Text
+          fw={400}
+          fz={{ base: '16px', md: '32px' }}
+          ta={{ base: 'left', md: 'center' }}
+          c="black"
+          mt="1rem"
+          style={{
+            opacity: visible ? 1 : 0,
+            transform: visible ? 'translateY(0px)' : 'translateY(40px)',
+            transition: 'opacity 0.8s ease-out, transform 0.8s ease-out',
+          }}
+          maw={{ sm: '687px' }}>
           {t('description_two')}
         </Text>
       </Stack>
